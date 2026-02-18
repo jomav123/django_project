6226fc8980384ebb5ffc71fabb6e04b2178fc451
jo mav <jo@gmail.com>
2026-02-18 11:53:56 +0100

    update

---
+++ README.md
@@ -0,0 +1,2 @@
+# cloudcourse
+welcome
---
+++ django/first_website/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FirstWebsiteConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'first_website'
